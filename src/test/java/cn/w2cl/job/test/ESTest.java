ce9a64e4151fa671e8c8fabbf5ea37eb88dc1b05
wei_jun9158 <jj67967903>
2020-02-01 12:07:32 +0800

    集成Elasticsearch 5.6.8    注意：集成5.6.8的时候springboot版本要2.0.x才可以，使用2.1.6就会报错
    Signed-off-by: wei_jun9158 <jj67967903>

---
+++ src/test/java/cn/w2cl/job/test/ESTest.java
@@ -0,0 +1,19 @@
+package cn.w2cl.job.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Authror 卫骏
+ * @Date 2020/2/1 11:29
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ESTest {
+    @Test
+    public void test01(){
+        System.out.println("123");
+    }
+}
